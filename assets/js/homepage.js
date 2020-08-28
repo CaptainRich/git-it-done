a389719a8447eaad3cfa11ac77c767740d3e8ae5
RichA <rich_ay@sbcglobal.net>
2020-08-28 16:23:54 -0500

    Added variable user name to Git request.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,18 @@
 // Javascript file for the HomePage
 
-var getUserRepos = function() {
-    fetch( "https://api.github.com/users/octocat/repos");
+var getUserRepos = function ( user ) {
+
+    // Format the GitHub API URL.
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // Make the request.
+    var response = fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        })
+
+    });
+    console.log("outside");
 }
 
-getUserRepos();
+getUserRepos("CaptainRich");
